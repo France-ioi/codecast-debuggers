de0d968d8a15663b00d1bfd57cf45038848d35a1
Thomas Juster <juster.thomas@gmail.com>
2022-02-18 18:41:42 +0100

    test(runner): update test to reconstruct result

--- src/call-script.spec.ts
+++ src/call-script.spec.ts
@@ -1,6 +1,19 @@
 import { callScript } from './call-script'
+import { applyPatches, Patch, enablePatches } from 'immer'
+import { StepSnapshot } from './run-steps/runner'
+
+enablePatches()
 
 describe('callScript()', () => {
+  function reconstructResultFromSteps(steps: Patch[][]): StepSnapshot[] {
+    return steps.reduce((acc, patches) => {
+      const previous = acc[acc.length-1] ?? {}
+      const snapshot = applyPatches(previous, patches) as StepSnapshot
+      acc.push(snapshot)
+      return acc
+    }, [] as StepSnapshot[])
+  }
+
   it.each([
     './samples/c/hello_world.c',
     './samples/cpp/function_rec.cpp',
@@ -9,6 +22,8 @@ describe('callScript()', () => {
     // './samples/php/hello_world.php',
   ])('should match snapshot for %s', async (fileRelativePath) => {
     const stringified = await callScript(fileRelativePath, 'off')
-    expect(JSON.parse(stringified)).toMatchSnapshot()
+    const steps = JSON.parse(stringified) as Patch[][]
+    const result = reconstructResultFromSteps(steps)
+    expect(result).toMatchSnapshot()
   })
 })
