05194c43c3f3c8a33b783235f30dbd7362584d7c
Thomas Juster <juster.thomas@gmail.com>
2022-03-08 10:26:49 +0100

    refactor(reconstruct): import same function in tests and debug script

--- src/reconstruct-snapshots.ts
+++ src/reconstruct-snapshots.ts
@@ -3,7 +3,7 @@ import { Steps, StepSnapshot } from './run-steps/runner';
 
 enablePatches();
 
-export function reconstructResultFromSteps(steps: Steps): StepSnapshot[] {
+export function reconstructSnapshotsFromSteps(steps: Steps): StepSnapshot[] {
   return steps.reduce((acc, patches) => {
     const previous = acc[acc.length-1] ?? {};
     const snapshot = applyPatches(previous, patches) as StepSnapshot;
