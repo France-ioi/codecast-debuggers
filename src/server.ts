bd68d19fb48c6671f2fa43d75e6a229aa07d1ec3
Sébastien Tainon <arkandias1@hotmail.fr>
2023-11-17 12:30:56 +0100

    Return details of syntax error instead of returning "terminated"

--- src/server.ts
+++ src/server.ts
@@ -78,7 +78,7 @@ function main(): void {
           success: true,
           snapshot: {
             terminated: true,
-            terminatedReason: message.type != 'end' ? message.type : undefined,
+            terminatedReason: message.type !== 'end' ? message.message : undefined,
           },
         },
       } as RemoteExecutionServerPayload));
